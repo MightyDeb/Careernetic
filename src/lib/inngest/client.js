63cbf3b66e4fecaf4be97efc7fad12be78c5f134
MightyDeb <debapriyamaity2005@gmail.com>
2025-02-22 14:29:59 +0530

    Cron for Weekly Insights ( Inngest Function )

--- src/lib/inngest/client.js
+++ src/lib/inngest/client.js
@@ -1,4 +1,10 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "my-app" , name: "Careernetic"});
+export const inngest = new Inngest({ id: "my-app" , name: "Careernetic",
+  credentials: {
+    gemini:{
+      apiKey: process.env.GEMINI_API_KEY
+    }
+  }
+});
