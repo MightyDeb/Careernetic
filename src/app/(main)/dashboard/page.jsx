788b14f95605ccad796c68d71e620d20d8a43f7d
MightyDeb <debapriyamaity2005@gmail.com>
2025-02-22 11:26:13 +0530

    created onboarding page and update profile api

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -1,6 +1,12 @@
+import { redirect } from 'next/navigation'
 import React from 'react'
+import { getUserOnboardingStatus } from '../../../../actions/user'
 
-const IndustryInsightsPage = () => {
+const IndustryInsightsPage = async() => {
+  const {isOnboarded}=await getUserOnboardingStatus()
+    if(!isOnboarded){
+      redirect("/onboarding")
+    }
   return (
     <div> IndustryInsightsPage</div>
   )
--- src/app/(main)/onboarding/page.jsx
+++ src/app/(main)/onboarding/page.jsx
@@ -1,8 +1,14 @@
 import { industries } from '@/data/industries'
 import React from 'react'
+import { getUserOnboardingStatus } from '../../../../actions/user'
+import { redirect } from 'next/navigation'
+import OnboardingForm from './_components/onboarding-form'
 
-const page = () => {
-  //
+const page = async() => {
+  const {isOnboarded}=await getUserOnboardingStatus()
+  if(isOnboarded){
+    redirect("/dashboard")
+  }
   return (
     <main>
       <OnboardingForm industries={industries}/>
